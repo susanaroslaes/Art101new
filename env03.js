351e68b52a1992e584a288fda6ab3aab580fffe0
susanaroslaes <susanarosales980@gmail.com>
2026-05-02 20:18:37 -0700

    lab 4 taks 2

---
+++ env03.js
@@ -0,0 +1,13 @@
+let environmentTitle = "The Neverbeast";
+
+let environmentElements = ["paws", "tail", "horns", "eyes"];
+
+let mainEntity = {
+    name: "Gruff",
+    type: "soft border",
+    mood: "intimidating",
+    isMoving: true,
+    favoriteElement: environmentElements[1]
+};
+
+
